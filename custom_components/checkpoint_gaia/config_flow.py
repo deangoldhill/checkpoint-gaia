e806f77438a4447cc948c3172c8fd70df760bc9e
deangoldhill <deangoldhill@gmail.com>
2026-02-22 13:25:06 +0000

    Create config_flow.py

---
+++ custom_components/checkpoint_gaia/config_flow.py
@@ -0,0 +1,22 @@
+import voluptuous as vol
+from homeassistant import config_entries
+from .const import (
+    DOMAIN, CONF_HOST, CONF_USERNAME, 
+    CONF_PASSWORD, CONF_PORT, CONF_VERIFY_SSL
+)
+
+class CheckPointGaiaConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
+    VERSION = 1
+
+    async def async_step_user(self, user_input=None):
+        if user_input is not None:
+            return self.async_create_entry(title=user_input[CONF_HOST], data=user_input)
+
+        data_schema = vol.Schema({
+            vol.Required(CONF_HOST): str,
+            vol.Required(CONF_USERNAME): str,
+            vol.Required(CONF_PASSWORD): str,
+            vol.Optional(CONF_PORT, default=443): int,
+            vol.Optional(CONF_VERIFY_SSL, default=False): bool,
+        })
+        return self.async_show_form(step_id="user", data_schema=data_schema)
